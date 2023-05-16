8b67c8f2dcb3f062cbe98fa424d7b5c80f17db61
Gabriel Dutra <gabrielcbdutra@gmail.com>
2023-05-16 09:30:46 -0300

    created the repository, but having an error when inject the depedence in controller

--- cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
+++ cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,16 +1,27 @@
 package com.example.cardapio.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
 
+	public FoodController() {
+		FoodRepository fp;
+	}
+	
 	@GetMapping
-	public void getAll() {
+	public List<Food> getAll() {
+		//List<Food> foodList = repository.findAll();
 		
+		return null;
 	}
 	
 }
