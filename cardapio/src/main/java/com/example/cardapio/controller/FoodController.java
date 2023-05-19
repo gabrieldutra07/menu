df9990b3982dd4f2dd7d49fb5298fb8738e31b5c
Gabriel Dutra <gabrielcbdutra@gmail.com>
2023-05-18 21:28:23 -0300

    adjust in controller

--- cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
+++ cardapio/src/main/java/com/example/cardapio/controller/FoodController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.cardapio.food.Food;
 import com.example.cardapio.food.FoodResponseDTO;
 import com.example.cardapio.repository.FoodRepository;
 
@@ -21,7 +20,7 @@ public class FoodController {
 	@GetMapping
 	public List<FoodResponseDTO> getAll() {
 		
-		List<Food> foodList = repository.findAll().stream().map(FoodResponseDTO::new);
+		List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
 		
 		return foodList;
 	}
